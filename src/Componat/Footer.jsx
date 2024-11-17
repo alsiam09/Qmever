81121fe02ab3a795bf014446b919998a399ef887
albaba projects <gmalsiam4200@gmail.com>
2024-11-18 04:52:49 +0600

    Glittrld

---
+++ src/Componat/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <section className='bg-[#062919]' >
+        <div className="container h-[400px] ">
+        </div>
+    </section>
+  )
+}
+
+export default Footer
