70eb785ce8c9376eb18d1e95f33ce5e62c738ff3
albaba projects <gmalsiam4200@gmail.com>
2024-11-18 13:54:34 +0600

    Glittrld

--- src/Componat/ContextApi.jsx
+++ src/Componat/ContextApi.jsx
@@ -2,6 +2,11 @@ import React, { createContext, useEffect, useState } from 'react';
 import axios from 'axios';
 import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
 import 'react-loading-skeleton/dist/skeleton.css'
+import { useParams, useResolvedPath } from 'react-router-dom';
+import { IoLogoWhatsapp } from "react-icons/io5";
+import { TfiEmail } from "react-icons/tfi";
+
+
 let apiData = createContext();
 
 const ContextApi = ({ children }) => {
@@ -9,7 +14,6 @@ const ContextApi = ({ children }) => {
   let [loading, setLoading] = useState(true);
   let [page, setPage] = useState(1);
   const itemsPerPage = 9; // Adjust this to load more or fewer items
-
   const getData = async () => {
     try {
       const res = await axios.get("https://rupkotha-a706e-default-rtdb.asia-southeast1.firebasedatabase.app/products.json");
@@ -21,6 +25,8 @@ const ContextApi = ({ children }) => {
     }
   };
 
+  let path = window.location.pathname
+  
   useEffect(() => {
     getData();
   }, [page]);
@@ -30,9 +36,10 @@ const ContextApi = ({ children }) => {
   
   const handleScroll = () => {
     
-    if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - window.innerHeight && !loading) {      
+    if (window.innerHeight + document.documentElement.scrollTop >= document.documentElement.offsetHeight - 300 && !loading) {      
       setPage(prevPage => prevPage + 1);
     } else{
+      setPage(prevPage => prevPage + 0);
     }
   };
 
@@ -50,7 +57,7 @@ const ContextApi = ({ children }) => {
   return (
     <apiData.Provider value={info}>
       {children}
-      {loading !== false && <div className=" container mx-auto ">
+      {loading ? <div className=" container mx-auto ">
         <div className=" my-[40px] ">
                 <SkeletonTheme className='' baseColor="#d4d4d4" highlightColor="#444">
     <p>
@@ -58,9 +65,13 @@ const ContextApi = ({ children }) => {
     </p>
   </SkeletonTheme>
       </div>
-      </div>} {/* Optional loading indicator */}
+      </div>:""} {/* Optional loading indicator */}
       <section className='bg-[#062919]' >
-        <div className="container h-[400px] ">
+        <div className="container mx-auto px-[10px] lg:px-[0px] py-[30px] ">
+          <h5 className=' flex items-center text-[#fff] md:text-[30px] font-[700]' >Qmever.com{path}</h5>
+          <h5 className=' px-[20px] h-[60px] cursor-pointer border-[#fff] border-[2px] md:w-[500px] w-[100%] rounded-[5px] flex items-center text-[#fff] md:text-[30px] font-[500] my-[20px] justify-between ' > <span className=' text-[23px] md:text-[30px] border-r-[2px] border-r-[red] pr-[20px] '><IoLogoWhatsapp/></span> <span className='text-[red] cursor-pointer '>+880 1813-904257</span></h5>
+          <h5 className=' px-[20px] h-[60px] cursor-pointer border-[#fff] border-[2px] md:w-[500px] w-[100%] rounded-[5px] flex items-center text-[#fff] md:text-[30px] font-[500] my-[20px] justify-between ' > <span className=' text-[23px] md:text-[30px] border-r-[2px] border-r-[red] pr-[20px] '><TfiEmail /></span> <span className='text-[red] cursor-pointer '>qmever.bd@gmail.com</span></h5>
+          <iframe className='w-[100%] h-[200px]' src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d117618.22871571548!2d89.17929430664063!3d22.892350670537734!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39ff6f45dabacc0f%3A0xa8030a8113dddfb!2sSiam%20House!5e0!3m2!1sen!2sbd!4v1731901565799!5m2!1sen!2sbd"></iframe>
         </div>
     </section>
     </apiData.Provider>
